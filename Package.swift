3b64ce45e57e632425227079daa18a762c161998
Koji Murata <koji.murata@dena.com>
2020-01-08 17:01:23 +0900

    package

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "URLQueryItemsCoder",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "URLQueryItemsCoder",
-            targets: ["URLQueryItemsCoder"]),
+        .library(name: "URLQueryItemsCoder", targets: ["URLQueryItemsCoder"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "URLQueryItemsCoder",
-            dependencies: []),
-        .testTarget(
-            name: "URLQueryItemsCoderTests",
-            dependencies: ["URLQueryItemsCoder"]),
+        .target(name: "URLQueryItemsCoder", dependencies: []),
+        .testTarget(name: "URLQueryItemsCoderTests", dependencies: ["URLQueryItemsCoder"]),
     ]
 )
